c9c7231892a83a0112b83a1d46ce268ac8bc9d05
Lucas Aquiles <lucasaquiles@gmail.com>
2018-04-17 12:08:56 -0300

    melhorias no grafico de acmpanhamento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416121725) do
+ActiveRecord::Schema.define(version: 20180417114506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20180416121725) do
   create_table "relevances", force: :cascade do |t|
     t.string "description"
     t.integer "value"
-    t.datetime "created_at", null: false  
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
